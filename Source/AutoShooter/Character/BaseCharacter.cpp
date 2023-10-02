f625f25723d94f34dede8bee8e5c535b007c52d6
Khaled Yousef <khaledyousef196@gmail.com>
2023-10-02 10:59:13 +0300

    Attributes Attachment to Character

---
+++ Source/AutoShooter/Character/BaseCharacter.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseCharacter.h"
+
+#include "AttributesComponent.h"
+
+
+// Sets default values
+ABaseCharacter::ABaseCharacter()
+{
+	AttributesComponent = CreateDefaultSubobject<UAttributesComponent>(TEXT("Attributes"));
+}
+
+
+void ABaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
